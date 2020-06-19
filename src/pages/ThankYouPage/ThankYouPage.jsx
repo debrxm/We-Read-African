c3da1f7911f6278651ea8126c579f6d1a7769ce4
james <jamesakpan016@gmail.com>
2020-06-19 19:55:02 +0100

    thank you page, contact page made responsive

---
+++ src/pages/ThankYouPage/ThankYouPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ThankYouPage.scss';
+
+const ThankYouPage = (props) => { 
+
+  return ( 
+    <div id='thankyouPageContaioner'>
+      <h1 id='heading'>Thank You!</h1>
+      <p>
+      You're now part of the tribe. An email will be sent to you shortly. No email? Be sure to check your junk mail for your FREE downloadable bookmark, it may be hiding there. 
+      </p>
+      <div className="social">
+        <a href='https://www.pinterest.co.uk/wereadafrican/'></a>
+        <a href='https://www.instagram.com/wereadafrican'></a>
+        <a href='https://www.twitter.com/wereadafrican'></a>
+        <a href='https://www.facebook.com/wereadafrican'></a>
+
+      </div>
+    </div>
+  )
+}
+
+export default ThankYouPage;
